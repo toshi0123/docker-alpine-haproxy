352a49d2bafd20ee08e0f4fe8579c72ff251e5b2
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-16 11:34:50 +0900

    Fixed the order of haproxy options

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-trap "echo 'reloading haproxy cfg';haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg && haproxy -f /usr/local/etc/haproxy/haproxy.cfg -p /run/haproxy.pid -sf \$(cat /run/haproxy.pid)" 1
+trap "echo 'reloading haproxy cfg';haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg && haproxy -p /run/haproxy.pid -f /usr/local/etc/haproxy/haproxy.cfg -sf \$(cat /run/haproxy.pid)" 1
 
 haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg || exit 1
 
